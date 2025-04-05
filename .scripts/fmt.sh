ffed66f44c8f49cd52806efdeb44d6c2a30cbfb2
2301_79425991 <2301_79425991@noreply.gitcode.com>
2025-04-05 08:39:21 +0800

    feat: 修改fmt.sh

--- .scripts/fmt.sh
+++ .scripts/fmt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # shellcheck disable=SC2034
-PROJECT_PREFIX="github.com/TimeWtr/shortlink-platform"
+PROJECT_PREFIX="github.com/TimeWtr"
 
 #shellcheck disable=SC2034
 for item in $(find . -type f -name "*.go" -not -path "./.idea/");
 do
-  goimports -l -w $"item";
+  goimports -l -w "$item";
 done
